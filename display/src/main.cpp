aa289788cfe208439cbdc6d8e4e3863d5e9044c7
Josh Wolfe <thejoshwolfe@gmail.com>
2012-08-25 21:16:59 -0700

    drawing a ship (a rectangle)

--- display/src/main.cpp
+++ display/src/main.cpp
@@ -12,15 +12,28 @@
 #include <GL/glut.h>
 #include <GL/glu.h>
 
-void display() { /* empty function   required as of glut 3.0 */ }
+float x, y;
 
-int main(int argc, char *argv[])
-{
+void display() {
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+    glColor3ub(0xff, 0xb6, 0xc1);
+    glRectf(x - 0.1f, y + 0.1f, x + 0.1f, y - 0.1f);
+
+    glutSwapBuffers();
+}
+
+int main(int argc, char *argv[]) {
     glutInit(&argc, argv);
     glutInitDisplayMode(GLUT_RGB | GLUT_DEPTH | GLUT_DOUBLE);
-    glutInitWindowSize(800,600);
+    glutInitWindowSize(800, 600);
     glutCreateWindow("Hello World");
     glutDisplayFunc(display);
+
+    glClearColor(0, 0, 0, 0);
+
+    x = 0;
+    y = 0;
     glutMainLoop();
     return 0;
 }
